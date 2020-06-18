6ac7df7456352b4f509300fdc4c75d8d144eb74e
puckish-debugger-0490 <jhidalgo521@gmail.com>
2020-06-18 00:34:36 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,15 +14,15 @@
 
 def speak_to_grandma(phrase)
     if phrase == 'I LOVE YOU GRANDMA!'
-      puts 'I LOVE YOU TOO PUMPKIN!'
+      return 'I LOVE YOU TOO PUMPKIN!'
     elsif phrase == 'Hi Nana, how are you?'
-      puts  'HUH?! SPEAK UP, SONNY!'
+      return  'HUH?! SPEAK UP, SONNY!'
     elsif phrase == 'Hi!'
-      puts 'HUH?! SPEAK UP, SONNY!'
+      return 'HUH?! SPEAK UP, SONNY!'
     elsif phrase == 'WHAT DID YOU EAT TODAY?'
-      puts "NO, NOT SINCE 1938!"
+      return "NO, NOT SINCE 1938!"
     else phrase == 'WHAT?'
-      puts "NO, NOT SINCE 1938!"
+      return "NO, NOT SINCE 1938!"
     end
 end
 
