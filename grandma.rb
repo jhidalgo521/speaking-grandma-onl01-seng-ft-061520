080706a2692817ad82c818239947b03557b1bea7
puckish-debugger-0490 <jhidalgo521@gmail.com>
2020-06-18 00:49:17 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-    if phrase == 'I LOVE YOU GRANDMA!'
+    if phrase == phrase.upcase
       return 'I LOVE YOU TOO PUMPKIN!'
     elsif phrase == 'Hi Nana, how are you?'
       return  'HUH?! SPEAK UP, SONNY!'
