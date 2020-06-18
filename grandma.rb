ac99be3844e4ce6539b089a4309ffebc8462fcae
puckish-debugger-0490 <jhidalgo521@gmail.com>
2020-06-18 00:52:07 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,9 +13,9 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-    if phrase == phrase.upcase
+    if phrase == 'I LOVE YOU GRANDMA!'
       return 'I LOVE YOU TOO PUMPKIN!'
-    elsif phrase == 'Hi Nana, how are you?'
+    elsif phrase == 
       return  'HUH?! SPEAK UP, SONNY!'
     elsif phrase == 'Hi!'
       return 'HUH?! SPEAK UP, SONNY!'
