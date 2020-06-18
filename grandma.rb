89495072a3ea0d4c2ae997c208304d5f8bee27b9
puckish-debugger-0490 <jhidalgo521@gmail.com>
2020-06-18 00:55:04 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,13 +15,9 @@
 def speak_to_grandma(phrase)
     if phrase == 'I LOVE YOU GRANDMA!'
       return 'I LOVE YOU TOO PUMPKIN!'
-    elsif phrase == 
+    elsif phrase == phrase.downcase
       return  'HUH?! SPEAK UP, SONNY!'
-    elsif phrase == 'Hi!'
-      return 'HUH?! SPEAK UP, SONNY!'
-    elsif phrase == 'WHAT DID YOU EAT TODAY?'
-      return "NO, NOT SINCE 1938!"
-    else phrase == 'WHAT?'
+    else phrase == phrase.upcase
       return "NO, NOT SINCE 1938!"
     end
 end
